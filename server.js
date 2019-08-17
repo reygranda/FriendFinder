ac638376b363e6251213bba22684eb85b4afcb85
reygranda <rey030299@gmail.com>
2019-08-17 14:57:06 -0400

    changes

---
+++ server.js
@@ -0,0 +1,24 @@
+let bodyParser = require("body-parser");
+let express = require("express");
+let path = require("path");
+
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+require('./app/routing/apiRoutes.js')(app);
+
+require('./app/routing/htmlRoutes.js')(app);
+
+
+app.listen(PORT, function(){
+    console.log(`App is listening on PORT: ${PORT}`)});
